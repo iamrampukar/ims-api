8ca467565f451e0f115b3a776ef370cd6fad0d3a
Ram Pukar <ram_mosone@yahoo.com>
2024-06-18 16:43:41 +0545

    [FIXED]:: Controller Response function added controller.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,28 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Response;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function sendResponse($response)
+    {
+        $response           =   ['success' => true] + $response;
+        $response['code']   =   200;
+        return Response::json($response);
+    }
+
+    /**
+     * @param $error
+     * @param int $code
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function sendError($response, $code = 404)
+    {
+        $response           = ['success' => false] + $response;
+        $response['code']   =   $code;
+        return Response::json($response, $code);
+    }
 }
